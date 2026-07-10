61caedbcb76e3282c9eff8c5e03ae80a73f14b4a
PIYUSH JOSHI <piyushjoshi9795@gmail.com>
2026-07-10 12:09:51 +0530

    All API's

---
+++ src/services/noteService.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL= 'http://localhost:5000/api/notes';
+
+export const getALLNotes= async()=>{
+    const res=await axios.get(API_URL);
+    return res.data;
+}
+
+export const createNote= async(noteData)=>{
+    const res=await axios.post(API_URL,noteData);
+    return res.data;
+}
+
+export const updateNote= async(id,noteData)=>{
+    const res=await axios.put(`$(API_URL)/${id}`,noteData);
+    return res.data;
+}
+
+export const deleteNote=async(id)=>{
+ await axios.delete(`$(API_URL)/${id}`);
+}
